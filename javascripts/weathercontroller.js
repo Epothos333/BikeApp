b081385d7b47e2c9f33ebeeea131080e6b81ab0a
Amanda Milke <amandalmilke@gmail.com>
2016-03-14 16:59:16 -0400

    final changes of Day 1

--- javascripts/weathercontroller.js
+++ javascripts/weathercontroller.js
@@ -1,5 +1,6 @@
 app.controller('bikeRoutes', ['$http', 'weatherService', '$scope', function($http, weatherService, $scope){
 	weatherService.then(function success(response){
 		$scope.posts = response.data.city.name;
+		$scope.temps = response.data.list[main];		
 	});
 }]);
