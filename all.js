0d98059423fab51e3add631c02551ea1bddf31e4
Your Name <deesmity7@yahoo.com>
2016-03-14 13:23:32 -0400

    routes

--- all.js
+++ all.js
@@ -1,1 +1,21 @@
-console.log('hi');
+var app = angular.module('bikeApp', ['ngRoute'])
+	app.config(function($routeProvider) {
+		$routeProvider.when("/",
+		{
+			templateUrl:"gettingStared.html",
+			controller: "gettingStared"
+		});
+
+		
+		$routeProvider.when("/bikeRoutes",
+		{
+			templateUrl:"bikeRoutes.html",
+			controller: "bikeRoutes"
+		});
+$routeProvider.when("/bikeMap",
+		{
+			templateUrl:"bikeMap.html",
+			controller: "BikeMap"
+		});
+
+	});
