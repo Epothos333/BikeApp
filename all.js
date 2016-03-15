c799b49383671a277d7dd729a2aa25b99fc19b8b
Edwin A <ea976@nyu.edu>
2016-03-15 18:03:53 -0400

    removed extra button on map view

--- all.js
+++ all.js
@@ -63,22 +63,14 @@ function initMap() {
 
 
 	});
-		var marker = new google.maps.Marker({
-		position: {
-			lat: 42.3404308730309, 
-			lng: -83.05515061325411
-		},
-		map: map,
-		title: 'Ayyyyyyye'
-	});
-		var marker = new google.maps.Marker({
-		position: {
-			lat: 42.330543, 
-			lng: -83.032071
-		},
-			map: map,
-			title: 'Detroit Wheel House'
-	});
+	// 	var marker = new google.maps.Marker({
+	// 	position: {
+	// 		lat: 42.330543, 
+	// 		lng: -83.032071
+	// 	},
+	// 		map: map,
+	// 		title: 'Detroit Wheel House'
+	// });
 
 		var bikeLayer = new google.maps.BicyclingLayer();
   		bikeLayer.setMap(map);
