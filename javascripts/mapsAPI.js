bbfc660eed7ad077a46d3dba10014841500b314e
Your Name <deesmity7@yahoo.com>
2016-03-15 18:12:08 -0400

    2nd Route

--- javascripts/mapsAPI.js
+++ javascripts/mapsAPI.js
@@ -47,6 +47,30 @@ function initMap() {
      });
 		    
 		    line.setMap(map);
+	}  
+
+		var line = new google.maps.Polyline({
+		    path: [{lat: 42.333066, lng: -83.043919},{lat: 42.350456, lng: -83.003704},{lat: 42.355661, lng: -83.007582},{lat: 42.369199, lng: -83.971909},{lat: 42.397077, lng: -83.990245},{lat: 42.98095, lng: -83.990347},{lat: 42.404954, lng: -83.997033,{lat: 42.334459, lng: -83.045552},{lat: 42.333138, lng: -83.043766}],
+		    geodesic: true,
+		    strokeColor: '#003366',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 2
+   
+     });
+		    
+		    line.setMap(map);
+	} 
+
+		 var line = new google.maps.Polyline({
+		    path: [{lat: 42.32900, lng: -83.050639},{lat: 42.369977, lng: -83.075277},{lat: 42.371626, lng: -83.071844},{lat: 42.340800, lng: -83.050214},{lat: 42.338136, lng: -83.057252}],
+		    geodesic: true,
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 2
+   
+     });
+		    
+		    line.setMap(map);
 	}
 
 		    return initMap
