53e396c5b8bae379bd46f59b60b403c13ceec2be
Edwin A <ea976@nyu.edu>
2016-03-17 09:33:22 -0400

    refactoring google API code

--- all.js
+++ all.js
@@ -143,6 +143,10 @@ app.factory('mapData', function(){
 				path: google.maps.SymbolPath.CIRCLE,
 				scale: 10
 	    };
+	    var bikeLayer = new google.maps.BicyclingLayer();
+
+
+
 function map2() {
 	var mapTwo = new google.maps.Map(document.getElementById('themap'), {
 		center: {
@@ -152,73 +156,7 @@ function map2() {
 		zoom: 11,
 		mapTypeId: google.maps.MapTypeId.TERRIAN
 	});
-}
-
-function map1() {
-
-
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: 42.3404308730309, 
-			lng: -83.05515061325411
-		},
-		zoom: 12,
-		 mapTypeId: google.maps.MapTypeId.TERRAIN
-	});
-		//Class for creating markers
-		var CreateMark = function(latit, lngit, title) {
-			var vm = this;
-			vm.marker = new google.maps.Marker({
-				position: {
-					lat: latit,
-					lng: lngit
-				},
-				icon: goldStar,
-				map: map,
-				title: title
-			});
-		};
-
-	    var detroitWH = new CreateMark(42.330543, -83.032071, 'Detroit Wheel House'),
-	     zagSter1 = new CreateMark(42.330640, -83.046645, 'Bike Rental Station at 611 Woodward'),
-	     zagSter2 = new CreateMark(42.331165, -83.048808, 'Bike Rental Station at 730 Shelby '),
-	     zagSter3 = new CreateMark(42.330984, -83.043208, 'Bike Rental Station at 160 E Congress'),
-	     zagSter4 = new CreateMark(42.328996, -83.045499, 'Bike Rental Station at 1 Woodward'),
-	     zagSter5 = new CreateMark(42.334633, -83.041486, 'Bike Rental Station at Greektown'),
-	     zagSter6 = new CreateMark(42.335645, -83.049324, 'Bike Rental at 1528 Woodward'),
-	     zagSter7 = new CreateMark(42.336298, -83.049400, 'Bike Rental at 1555 Boradway');
-
-
-		var bikeLayer = new google.maps.BicyclingLayer();
-  		bikeLayer.setMap(map);	
-		  var line = new google.maps.Polyline({
-		    path: [
-		    {
-		    	lat: 42.32900,
-		     	lng: -83.050639
-		     },
-		     {
-		     	lat: 42.369977,
-		     	 lng: -83.075277
-		     },
-		     {
-		     	lat: 42.371626,
-		     	lng: -83.071844
-		     },
-		     {
-		     	lat: 42.340800,
-		     	lng: -83.050214
-		     },
-		     {
-		     	lat: 42.338136, 
-		     	lng: -83.057252
-		     }],
-		    geodesic: true,
-		    strokeColor: '#FF0000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 2
-     });		    
-		    line.setMap(map);
+	bikeLayer.setMap(mapTwo);
 
 		var lineTwo = new google.maps.Polyline(
 		{
@@ -259,8 +197,81 @@ function map1() {
 		    strokeWeight: 2
    
      });
+		 lineTwo.setMap(mapTwo); 
+
+
+}
+
+function map1() {
+	var CreateMark = function(latit, lngit, title) {
+		var vm = this;
+		vm.marker = new google.maps.Marker({
+			position: {
+					lat: latit,
+					lng: lngit
+				},
+			icon: goldStar,
+			map: map,
+			title: title
+		});
+	};
+
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 42.3404308730309, 
+			lng: -83.05515061325411
+		},
+		zoom: 12,
+		 mapTypeId: google.maps.MapTypeId.TERRAIN
+	});
+		//Class for creating markers
+
+		
+	    var detroitWH = new CreateMark(42.330543, -83.032071, 'Detroit Wheel House'),
+	     zagSter1 = new CreateMark(42.330640, -83.046645, 'Bike Rental Station at 611 Woodward'),
+	     zagSter2 = new CreateMark(42.331165, -83.048808, 'Bike Rental Station at 730 Shelby '),
+	     zagSter3 = new CreateMark(42.330984, -83.043208, 'Bike Rental Station at 160 E Congress'),
+	     zagSter4 = new CreateMark(42.328996, -83.045499, 'Bike Rental Station at 1 Woodward'),
+	     zagSter5 = new CreateMark(42.334633, -83.041486, 'Bike Rental Station at Greektown'),
+	     zagSter6 = new CreateMark(42.335645, -83.049324, 'Bike Rental at 1528 Woodward'),
+	     zagSter7 = new CreateMark(42.336298, -83.049400, 'Bike Rental at 1555 Boradway');
+
+
+
+  		bikeLayer.setMap(map);	
+		  var line = new google.maps.Polyline({
+		    path: [
+		    {
+		    	lat: 42.32900,
+		     	lng: -83.050639
+		     },
+		     {
+		     	lat: 42.369977,
+		     	 lng: -83.075277
+		     },
+		     {
+		     	lat: 42.371626,
+		     	lng: -83.071844
+		     },
+		     {
+		     	lat: 42.340800,
+		     	lng: -83.050214
+		     },
+		     {
+		     	lat: 42.338136, 
+		     	lng: -83.057252
+		     }],
+		    geodesic: true,
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 2
+     });		    
+		    line.setMap(map);
+
+
 		    
-		    lineTwo.setMap(map); 
+		   
 		
 		var lineThree = new google.maps.Polyline(
 		{
