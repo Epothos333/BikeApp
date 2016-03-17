7ecd568af1565ae74722b3d5dab843a5dc68d38c
Your Name <deesmity7@yahoo.com>
2016-03-17 15:31:37 -0400

    changes

--- javascripts/directives/toggleDirective.js
+++ javascripts/directives/toggleDirective.js
@@ -6,7 +6,7 @@ app.directive('toggleClass', function() {
 			var btn = document.getElementById('butts');
 			btn.addEventListener("click", hideClass);
 			function hideShowClass(){
-				if()
+				
 				fds.classList.add('hide');					
 			}				
 		}
