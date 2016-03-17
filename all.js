85dfecc9818afbba28e2128ec2cc1f389a4800e4
Amanda Milke <amandalmilke@gmail.com>
2016-03-17 16:10:29 -0400

    added intermediate and advanced directive

--- all.js
+++ all.js
@@ -34,13 +34,6 @@ app.config(['$routeProvider',
 
 
 
-app.controller('intermediateController', ['mapData', '$scope', function(mapData, $scope) {
-	
-
-	return mapData.intMapOne();
-
-
-}]);
 app.controller('advancedController', ['mapData', '$scope', function(mapData, $scope) {
 	
 
@@ -60,6 +53,13 @@ app.controller('getStartCont', function($scope, $location) {
 		$location.path(view);
 	}
 });
+app.controller('intermediateController', ['mapData', '$scope', function(mapData, $scope) {
+	
+
+	return mapData.intMapOne();
+
+
+}]);
 app.controller('bikeRoutes', ['$http', 'weatherService', '$scope', '$location', 'mapData', function($http, weatherService, $scope, $location, mapData){
 	weatherService.then(function success(response){
 		$scope.printWeather = function() {
@@ -142,14 +142,62 @@ app.directive('googleMap', function() {
 })
 app.directive('toggleClass', function() {
 	return {
-		template: "<button id='butts'>Easy Route One</button>",
-		link: function($scope, element, attr){
-			var fds = document.getElementById('EZmapOne');
-			var btn = document.getElementById('butts');
-			btn.addEventListener("click", hideClass);
-			function hideShowClass(){
-
-				fds.classList.add('hide');					
+		template: "<button id='btn1'>Easy Route One</button><button id='btn2'>Easy Route Two</button><button id='btn3'>Easy Route 3</button>",
+		link: function(){
+			var map1 = document.getElementById('EZmapOne');
+			var map2 = document.getElementById('EZmapTwo');
+			var map3 = document.getElementById('EZmapThree');						
+			var btn1 = document.getElementById('btn1');
+			var btn2 = document.getElementById('btn2');
+			var btn3 = document.getElementById('btn3');						
+			btn1.addEventListener("click", btn1Show);
+			btn2.addEventListener("click", btn2Show);
+			btn3.addEventListener("click", btn3Show);						
+			function btn1Show(){
+				map2.classList.add('hide');	
+				map3.classList.add('hide');
+				map1.classList.remove('hide');				
+			}	
+			function btn2Show(){
+				map1.classList.add('hide');	
+				map3.classList.add('hide');	
+				map2.classList.remove('hide');			
+			}	
+			function btn3Show(){
+				map2.classList.add('hide');	
+				map1.classList.add('hide');		
+				map3.classList.remove('hide');	
+			}
+	}
+}
+})
+app.directive('intClass', function() {
+	return {
+		template: "<button id='btnint1'>Intermediate Route One</button><button id='btnint2'>Intermediate Route Two</button><button id='btnint3'>Intermediate Route 3</button>",
+		link: function(){
+			var map1 = document.getElementById('INTmapOne');
+			var map2 = document.getElementById('INTmapTwo');
+			var map3 = document.getElementById('INTmapThree');						
+			var btn1 = document.getElementById('btnint1');
+			var btn2 = document.getElementById('btnint2');
+			var btn3 = document.getElementById('btnint3');						
+			btnint1.addEventListener("click", btn1Show);
+			btnint2.addEventListener("click", btn2Show);
+			btnint3.addEventListener("click", btn3Show);						
+			function btn1Show(){
+				map2.classList.add('hide');	
+				map3.classList.add('hide');
+				map1.classList.remove('hide');				
+			}	
+			function btn2Show(){
+				map1.classList.add('hide');	
+				map3.classList.add('hide');	
+				map2.classList.remove('hide');			
+			}	
+			function btn3Show(){
+				map2.classList.add('hide');	
+				map1.classList.add('hide');		
+				map3.classList.remove('hide');		
 			}				
 		}
 
