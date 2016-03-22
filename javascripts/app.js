2f1280a6b72cb2ad7ce007c16bf1373b933d1a12
Amanda Milke <amandalmilke@gmail.com>
2016-03-22 14:33:40 -0400

    undoing changes

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,7 @@
 var app = angular.module('bikeApp', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+app.config(['$routeProvider',
+  function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: '/Views/gettingStarted.html',
         controller: 'getStartCont'
